7dabb10d11a2845e0b96bed8c9995821c8588b1a
Calvin Chen <calvin.chen@asml.com>
2023-07-28 09:50:37 +0800

    prepare pseudo

---
+++ baskerville/g++.sh
@@ -0,0 +1,34 @@
+
+
+
+load Module - FFmpeg/4.3.2-GCCcore-10.3.0  # 10.3.0
+
+
+
+
+# If you do not have administrative
+  #access to this machine, you may wish to choose a different installation
+  #directory, preferably one that is listed in your PYTHONPATH environment
+  #variable.
+
+
+python setup.py install  --prefix=/bask/projects/j/jiaoj-3d-vision/Hao/anaconda/envs/py37
+
+# or
+
+pip install pyproject-toml
+pip install .
+
+#  pip install Align1D
+
+python setup.py install --user
+
+
+# setup install failed with
+# ref to https://github.com/princeton-vl/DROID-SLAM/issues/20
+
+
+pip3 install --upgrade pip
+pip install  --upgrade setuptools
+
+
--- data/kaggle.sh
+++ data/kaggle.sh
@@ -3,17 +3,16 @@
 # Upload
 
 # 1.
-kaggle datasets init -p /path/to/dataset/
+kaggle datasets init -p .
 
 # 2.
 vim dataset-metadata.json
 
 # 3.
-kaggle datasets create -p /path/to/dataset/
+kaggle datasets create -p .
 
 # Update
-kaggle datasets version -p /path/to/dataset -m "Your message here"
-
+kaggle datasets version -p . -m "Your message here"
 
 # Download
 kaggle datasets download -d calvchen/bilateral
